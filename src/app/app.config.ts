28391397e61333ffea5dc8852fb4a0c5172bac7c
kshitiz <adhikarikshitiz12@gmail.com>
2025-01-03 22:01:49 +0545

    add moment js

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,9 +4,12 @@ import {provideRouter} from '@angular/router';
 import {routes} from './app.routes';
 import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
 import {provideHttpClient} from "@angular/common/http";
+import {provideMomentDateAdapter} from "@angular/material-moment-adapter";
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({eventCoalescing: true}),
     provideRouter(routes), provideAnimationsAsync(),
-    provideHttpClient()]
+    provideHttpClient(),
+    provideMomentDateAdapter()
+  ]
 };
