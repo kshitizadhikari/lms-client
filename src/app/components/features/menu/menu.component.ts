3cec9b10e86f3667582de5162e9b297e1d712686
kshitiz <adhikarikshitiz12@gmail.com>
2025-01-17 22:18:59 +0545

    use mat tabs to separate menu components

--- src/app/components/features/menu/menu.component.ts
+++ src/app/components/features/menu/menu.component.ts
@@ -1,10 +1,14 @@
 import {Component} from '@angular/core';
+import {MenuPlannerComponent} from "./menu-planner/menu-planner.component";
+import {MatTabsModule} from "@angular/material/tabs";
 import {MenuListComponent} from "./menu-list/menu-list.component";
 
 @Component({
   selector: 'app-menu',
   standalone: true,
   imports: [
+    MatTabsModule,
+    MenuPlannerComponent,
     MenuListComponent
   ],
   templateUrl: './menu.component.html',
