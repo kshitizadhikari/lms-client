cd8a57993351b78de6ea28e1be90eb4127574368
kshitiz <adhikarikshitiz12@gmail.com>
2025-01-17 22:19:51 +0545

    fetch all menus in menu-list

--- src/app/components/features/menu/menu-list/menu-list.component.ts
+++ src/app/components/features/menu/menu-list/menu-list.component.ts
@@ -1,87 +1,47 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
-import {ButtonComponent} from "../../../../shared/components/ui-components/button/button.component";
 import {Subscription} from "rxjs";
-import {NgForOf} from "@angular/common";
-import {MatFormField, MatFormFieldModule} from "@angular/material/form-field";
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import moment from "moment";
-import {MenuFormComponent} from "./menu-form/menu-form.component";
-import {FormsModule} from "@angular/forms";
-import {MenuModel, MenuQueryParameter} from "../../../../models/menu.model";
+import {MenuModel} from "../../../../models/menu.model";
 import {MenuService} from "../../../../services/menu.service";
+import {IncludeQPModel} from "../../../../models/params/include-qp.model";
 
 @Component({
   selector: 'app-menu-list',
   standalone: true,
-  imports: [
-    ButtonComponent,
-    NgForOf,
-    MatFormField,
-    MatDatepickerModule,
-    MatFormFieldModule,
-    MenuFormComponent,
-    FormsModule,
-  ],
+  imports: [],
   templateUrl: './menu-list.component.html',
   styleUrl: './menu-list.component.css'
 })
 export class MenuListComponent implements OnInit, OnDestroy {
+
   private sub: Subscription = new Subscription();
-  startDate = '';
-  endDate = '';
-  numberOfDays: number = 0;
-  days: any[] = [];
-  weeksMenu: MenuModel[] = [];
-  menuParam: MenuQueryParameter;
+  menus: MenuModel[] = [];
+  includeQP: IncludeQPModel;
 
   constructor(
-    private menuService: MenuService
+    private menuService: MenuService,
   ) {
-    this.menuParam = {
-      includeMenuItems: false
-    };
+    this.includeQP = {
+      menuItems: true,
+      foods: true
+    }
   }
 
   ngOnInit(): void {
-
+    this.loadAllMenus();
   }
 
-  getWeek() {
-    this.days = [];
-    const start = moment(this.startDate);
-    const end = moment(this.endDate);
-    this.numberOfDays = end.diff(start, 'days') + 1;
-    let currentDate = start.clone();
-    while (currentDate.isBefore(end) || currentDate.isSame(end)) {
-      this.days.push({
-        date: currentDate.format('YYYY-MM-DD'),
-        day: currentDate.format('dddd')
-      });
-      currentDate.add(1, 'days');
-    }
-    this.getWeeksMenu();
-  }
-
-  getWeeksMenu() {
-    this.weeksMenu = [];
-    // this.menuService.getAll().subscribe({
-    //   next: ((res) => {
-    //     this.weeksMenu = res;
-    //     console.log(this.weeksMenu);
-    //   }),
-    //   error: (err => {
-    //     console.log('Error fetching menu for the selected date range\n', err);
-    //   })
-    // });
-    this.menuParam.includeMenuItems = true;
-    this.menuService.getAllInclude(this.menuParam).subscribe({
-      next: ((res) => {
-
-      }),
-      error: ((err) => {
-        console.log('Error fetching menu');
+  loadAllMenus(): void {
+    this.sub.add(
+      this.menuService.getAll(this.includeQP).subscribe({
+        next: (res) => {
+          this.menus = res;
+          // console.log(this.menus);
+        },
+        error: (err) => {
+          console.log("Error fetching menus", err);
+        }
       })
-    });
+    )
   }
 
   ngOnDestroy(): void {
