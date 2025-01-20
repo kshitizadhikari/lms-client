eb36ae8bce08f61568525b5765baf4863f97e3bb
kshitiz <adhikarikshitiz12@gmail.com>
2025-01-20 23:11:16 +0545

    remove date range from menu-list component

--- src/app/components/features/menu/menu-list/menu-list.component.ts
+++ src/app/components/features/menu/menu-list/menu-list.component.ts
@@ -3,11 +3,16 @@ import {Subscription} from "rxjs";
 import {MenuModel} from "../../../../models/menu.model";
 import {MenuService} from "../../../../services/menu.service";
 import {IncludeQPModel} from "../../../../models/params/include-qp.model";
+import {SnackbarService} from "../../../../shared/services/snackbar.service";
+import {NgForOf} from "@angular/common";
+import moment from "moment";
 
 @Component({
   selector: 'app-menu-list',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf
+  ],
   templateUrl: './menu-list.component.html',
   styleUrl: './menu-list.component.css'
 })
@@ -19,6 +24,7 @@ export class MenuListComponent implements OnInit, OnDestroy {
 
   constructor(
     private menuService: MenuService,
+    private snackbar: SnackbarService
   ) {
     this.includeQP = {
       menuItems: true,
@@ -35,10 +41,10 @@ export class MenuListComponent implements OnInit, OnDestroy {
       this.menuService.getAll(this.includeQP).subscribe({
         next: (res) => {
           this.menus = res;
-          // console.log(this.menus);
+          console.log(this.menus);
         },
         error: (err) => {
-          console.log("Error fetching menus", err);
+          this.snackbar.error(err);
         }
       })
     )
@@ -47,4 +53,6 @@ export class MenuListComponent implements OnInit, OnDestroy {
   ngOnDestroy(): void {
     this.sub.unsubscribe();
   }
+
+  protected readonly moment = moment;
 }
