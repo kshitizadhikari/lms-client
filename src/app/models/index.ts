061eea2d83de3d5e9969a76c096884a0e68052ea
kshitiz <adhikarikshitiz12@gmail.com>
2025-01-21 23:45:24 +0545

    create index.ts file for exporting models

---
+++ src/app/models/index.ts
@@ -0,0 +1,6 @@
+export * from './customer.model';
+export * from './date-range.model';
+export * from './enum';
+export * from './error.model';
+export * from './menu.model';
+export * from './person.model';
